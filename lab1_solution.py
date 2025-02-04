308c04c27586d2895fb63d52ec3603196a1e6468
Sabreen <sabrmbins@gmail.com>
2025-02-04 11:52:49 +0300

    Lab 1 solved

---
+++ lab1_solution.py
@@ -0,0 +1,21 @@
+'''Sabreen Binsalman'''
+
+#Define a variable that has a value of your favourite football team.
+my_team= "Al Ittihad"
+print("My favourite football team is ",my_team)
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+x=7
+y=5.5
+
+print("an integer number x =",x)
+print("a float number y =",y)
+
+#Assign One Value to Multiple Variables (choose random names & values).
+num1 = num2 = num3 = 30
+print("all three values are the same : \nValue 1 =",num1,"\nValue 2 =", num2,"\nValue 3 =",num3)
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+num4, num5 = 22,10
+
+print("value 4:",num4 ,"is grater than value 5:",num5)
+
