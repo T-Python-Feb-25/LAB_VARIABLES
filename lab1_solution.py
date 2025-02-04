9a269b361c16456ffe2a52e7073ac306723acf13
Sabreen <sabrmbins@gmail.com>
2025-02-04 12:28:28 +0300

    modified version

--- lab1_solution.py
+++ lab1_solution.py
@@ -8,14 +8,14 @@ x=7
 y=5.5
 
 print(f"an integer number x ={x}")
-print("a float number y =",y)
+print(f"a float number y ={y}")
 
 #Assign One Value to Multiple Variables (choose random names & values).
 num1 = num2 = num3 = 30
-print("all three values are the same : \nValue 1 =",num1,"\nValue 2 =", num2,"\nValue 3 =",num3)
+print(f"all three values are the same : \nValue 1 ={num1}\nValue 2 ={num2}\nValue 3 ={num3}")
 
 #Assign multiple Values to Multiple Variables (choose random names & values).
 num4, num5 = 22,10
 
-print("value 4:",num4 ,"is grater than value 5:",num5)
+print(f"value 4:{num4} is grater than value 5:{num5}")
 
