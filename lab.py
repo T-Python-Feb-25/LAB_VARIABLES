5715ef795f845e4f20009137033d909387cf6408
Elaf-Abdulrhman <160735795+Elaf-Abdulrhman@users.noreply.github.com>
2025-02-04 12:05:28 +0300

    Create lab.py

---
+++ lab.py
@@ -0,0 +1,19 @@
+# Define a variable with my favorite football team
+favorite_team = "Ittihad"
+
+# Define a variable 'x' with an integer value and 'y' with a float value
+x = 14
+y = 10.5
+
+# Assign one value to multiple variables
+team1 = team2 = team3 = "Real Madrid"
+
+# Assign multiple values to multiple variables
+player, age, goals = "Lionel Messi", 34, 672
+
+# Print the values
+print("Favorite Football Team:", favorite_team)
+print("x (Integer Value):", x)
+print("y (Float Value):", y)
+print("Assigned Values to Multiple Variables: ", team1, team2, team3)
+print("Player:", player, "| Age:", age, "| Goals:", goals)
