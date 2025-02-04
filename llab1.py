a401e1792f8b63023e9dd15f1f1b855633fee7f8
raghad algarni <raghad.algarni601@gmail.com>
2025-02-04 13:20:55 +0300

    finish the lab1

---
+++ llab1.py
@@ -0,0 +1,16 @@
+
+
+MyFavoriteTeam = "nasser"
+
+
+x:int= 10
+y:float = 8.9
+
+
+# one value to multiple variables
+num_academy = twaiq = 1
+
+
+# multiple values to multiple variables
+num, Age= 0, 4
+print(num, Age, num_academy, twaiq, MyFavoriteTeam)
