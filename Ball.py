801b15332be36152f8ad6666ab0c4e63811ee8ab
Areej <areej.a7676@gmail.com>
2025-02-04 12:35:23 +0300

    Add favorite football team and define player variables

---
+++ Ball.py
@@ -0,0 +1,14 @@
+favorite_team = "FC Barcelona"
+
+#متغيرات عدد صحيح 
+
+x = 77  #int v
+y = 7.77  #fioat v
+
+a = b = c = "Football"
+
+name1, age1, heighit1 = "Alice", 25,  5.7
+name2, age2, heighit2 = "Bop", 30,  6.0
+
+
+
