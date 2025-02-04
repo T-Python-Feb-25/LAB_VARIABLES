63403991b33cb374984bcb0fd9a89ed50b8d81eb
wijdan <wjdanalbdalzyz@gmail.com>
2025-02-04 13:06:15 +0300

    add solution lab varible

---
+++ python1.py
@@ -0,0 +1,15 @@
+Best_team = "Al-Nassr"
+x = 10
+y = 5.5
+
+a = m = w = "shared value"
+
+name1 , name2 , name3 = "ahmad" , "fares" , "saad"
+
+
+
+print ("Best_team : ",Best_team)
+print ("x",x)
+print ("y",y)
+print("a",a , "m",m , "w",w)
+print("namess:",name1,name2,name3)
