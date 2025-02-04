d1ebc0f7b2f9f7ecdd182da2992213a88bcf6e87
manaralamri <manaralamre33@gmail.com>
2025-02-04 11:48:06 +0300

    Solution LAP1

---
+++ main.py
@@ -0,0 +1,13 @@
+favourite_football = "Real Madrid"
+
+x:int = 2
+y:float = 3.5
+
+a = b = c = "Hello, Python!"
+
+name , age , country = "Manar" , "22", "Saudi"
+
+print(favourite_football)
+print(x, y)
+print(a , b, c)
+print(name , age , country)
