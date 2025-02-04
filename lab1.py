2d66061d2bb3107597bab6f97c46838767ed9d8e
Nawaf Alahmadi <nawaf.m.alahmadi@gmail.com>
2025-02-04 12:34:16 +0300

    Upload First Lab In Python

---
+++ lab1.py
@@ -0,0 +1,12 @@
+# First Task
+favFootballTeam = "Al-Ittihad"
+
+# Second Task
+x = 10
+y = 10.50
+
+# Third Task
+var1 = var2 = var3 = "One Value For Multiple Variables"
+
+# Fourth Task
+var1 ,var2, var3 = "Value 1" , "Value 2" ,"Value 3"
