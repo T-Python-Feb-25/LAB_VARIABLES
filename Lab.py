ccf8e1921c0f95c594f32b9314eee4fbdca8ccea
Ghaida <ghaidagh347@gmail.com>
2025-02-04 12:30:46 +0300

    committ the change

---
+++ Lab.py
@@ -0,0 +1,28 @@
+# LAB_VARIABLES
+
+# Define a variable that has a value of your favourite football team.
+football_team = "Al-Hilal"
+print("My favourite football team is ", football_team)
+
+
+
+
+# Define a variable “x“  has an integer value.
+x:int= 3
+print("Variable “x“ type is" ,type(x), "and it's equal",x)
+
+
+#  Define a variable “y” has a float value.
+y:float= 7.50
+print(f"Variable “y” type is {type(y)} and it's equal {y}")
+
+
+
+# Assign One Value to Multiple Variables. 
+L1= L2= L3 = 24 
+
+print("(L1, L2 and L3) they have the same value which is", L1)
+
+# Assign multiple Values to Multiple Variables 
+mv1, mv2, mv3 = True, True, False
+print(f"mv1={mv1} , mv2={mv2} and mv3={mv3}")
