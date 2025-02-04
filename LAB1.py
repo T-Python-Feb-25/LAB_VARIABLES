b69c8d1266cf5a4ade1c26d3e74ae6ccdb3fba71
Ahmed Alghamdi <moodya793@gmail.com>
2025-02-04 13:21:00 +0300

    Fav Team , vari Y and X , 3 Vari oneline and one value, 3 Vari with 3 value.

---
+++ LAB1.py
@@ -0,0 +1,22 @@
+
+
+# The fav team.
+Team = "alnasser"
+
+# Variable 'Y'
+x : int = 22
+
+# Variable 'x'
+y : float = 11
+
+# 3 Variables in one value 
+name0 = name1 = name2 = 21
+
+# 3 Variable with 3 value in oneline
+name4 , name5 , name6 = 90 , 77 , 30
+
+print(name2 + name5)
+print(name1 * name2 + name0 - name6)
+
+
+
