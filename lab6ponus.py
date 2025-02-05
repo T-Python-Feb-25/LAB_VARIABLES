51880752d4207a0e3b9c8d0be4253e98ab079296
rahaf fallatah <rahaf77553@gmail.com>
2025-02-05 14:37:41 +0300

    lab6ponus solution

---
+++ lab6ponus.py
@@ -0,0 +1,7 @@
+myInfo="my full name is rahaf ahmad fallatah i am from makkah"
+name="rahaf"
+print("the length of my info is {}".format(len(myInfo)))
+print("The number of times my name appears {}".format(myInfo.count(name)))
+print(myInfo.upper())
+print(myInfo.lower())
+print(myInfo[-1])
